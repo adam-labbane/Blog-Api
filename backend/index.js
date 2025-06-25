182e17d7fea06ee17c5f0bfd75020e3019cb0ed1
adam-labbane <adamlabbane.dev@gmail.com>
2025-06-25 17:28:25 +0200

    erreur mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,34 @@
 const express = require("express");
-const connectDB = require("./lib/db");
+const mongoose = require("mongoose");
 const Post = require("./models/Post");
 const cors = require("cors");
 
-const app = express();
 
-app.use(cors());
+const app = express();
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: "*"
+}));
+//const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/blogdb";
+
 app.use(express.json());
 
+mongoose.connect("mongodb+srv://adam:mazdour@cluster0.hrwrde2.mongodb.net")
+  .then(() => {
+    console.log("Connexion réussie à MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.error("Échec de la connexion :", err);
+  });
+
+// Routes
 app.get("/", (req, res) => {
   res.send("API Blog en ligne");
 });
 
-app.get("/posts", async (req, res) => {
-  try {
-    await connectDB(); 
-    const posts = await Post.find().sort({ createdAt: -1 });
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 app.post("/posts", async (req, res) => {
   try {
-    await connectDB();
     const newPost = await Post.create(req.body);
     res.status(201).json(newPost);
   } catch (err) {
@@ -32,4 +36,14 @@ app.post("/posts", async (req, res) => {
   }
 });
 
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+// const PORT = 8000;
+// app.listen(PORT, () => console.log(`Serveur démarré`));
 module.exports = app;
